24f7376a00382625c6ecde9359c4cb87e8417962
Arli Lima <arlilima@Arlis-MacBook-Pro.local>
2021-09-18 19:18:30 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
