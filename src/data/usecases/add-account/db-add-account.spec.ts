69600174ebd28f81b4be238f9ca95c314168d5d7
Arli Machado <arli.machado@gmail.com>
2023-07-18 18:40:26 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -95,20 +95,38 @@ describe('DBAddAccount Usecase', () => {
       password: 'hashed_password'
     })
   })
-})
+  test('Should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
 
-test('Should throw if AddAccountRepository throws', async () => {
-  const { sut, addAccountRepositoryStub } = makeSut()
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
 
-  jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
 
-  const accountData = {
-    name: 'valid_name',
-    email: 'valid_email',
-    password: 'valid_password'
-  }
+    const promise = sut.add(accountData)
 
-  const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
 
-  await expect(promise).rejects.toThrow()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+
+    })
+  })
 })
